da22a3c2a4474e0b50d63c6be10803aba7445a1e
Camilacat05 <37548708+Camilacat05@users.noreply.github.com>
2019-04-10 14:25:09 -0300

    servidor1 - caixa alta

--- index.js
+++ index.js
@@ -1,64 +1,16 @@
-const request = require('request'); //fazer requisições a servidores
-const threads = require('threads').spawn; //biblioteca para trabalhar com threads no node.js
+const express = require('express');
+const bodyParser = require('body-parser');
 
-class Palavra {
-    constructor() {
-    }
+const app = express();
 
-    async palavraRecebe(req, res) { //método que recebe a requisição do app, cria uma thread e abre um canal, dentro do canal envia duas requisições (servidor 1 e servidor 2), e após receber a resposta dos dois, retorna essa resposta para a aplicação cliente que fez a requisição.
-        const thread = threads((input, done) => { //cria a thread
-            done();
-        });
-        thread.send().on('message', async (response) => {
-            const palavra = req.body.palavra;
-            await this.pegaPalavras(palavra, res);
-            thread.kill(); // encerra a thread
-        })
-            .on('error', function (error) {
-                console.error('Worker errored:', error);
-            })
-            .on('exit', function () {// quando encerra a thread vem pra cá
-                console.log('Worker has been terminated.');
-            });
-    }
-    // envia as requisições aos dois servidores (chama os dois métodos responsáveis pelo envio das requisições)
-    async pegaPalavras(palavra, res) {
-        let palavraCaixaAlta = await this.caixaAlta(palavra);
-        let palavraConcatenada = await this.concat(palavra);
-        res.send({ palavraCaixaAlta: palavraCaixaAlta, palavraConcatenada: palavraConcatenada });
-    }
+app.use(bodyParser.json());
 
-    async caixaAlta(palavra) {
-        return new Promise((resolve, reject) => {
-            request.post('http://192.168.43.228:3001/servidor2', {
-                json: {
-                    palavra: palavra
-                }
-            }, (error, resp, body) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(body.palavra);
-                }
-            });
-        });
-    }
-    async concat(palavra) {
-//requisições ao servidores
-        return new Promise((resolve, reject) => {
-            request.post('http://192.168.43.21:3002/servidor3', {
-                json: {
-                    palavra: palavra
-                }
-            }, (error, resp, body) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(body.palavra);
-                }
-            });
-        });
-    }
-}
+app.post('/servidor2', (req, res) => {
+    let palavra = req.body.palavra;
+    palavra = palavra.toUpperCase(); // converte para maiúscula
+    res.send({ palavra: palavra });
+});
 
-export default Palavra;
+app.listen(3001, () => {
+    console.log("Servidor online!");
+});
